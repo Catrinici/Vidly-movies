0f19e7cea6cf99e79723161b10eab2166a5abc7b
Catrinici <catrinici.antonina@gmail.com>
2020-09-10 15:33:32 -0700

    adding login/reg

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
 import "./App.css";
 import LoginForm from './components/loginForm';
+import RegisterForm from './components/registerForm';
 
 class App extends Component {
   render() {
@@ -15,8 +16,9 @@ class App extends Component {
       <React.Fragment>
         <NavBar />
         <main className="container">
-					<Switch>
-						<Route path="/login" component={LoginForm} />
+          <Switch>
+            <Route path="/register" component={RegisterForm} />
+            <Route path="/login" component={LoginForm} />
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movies} />
             <Route path="/customers" component={Customers} />
